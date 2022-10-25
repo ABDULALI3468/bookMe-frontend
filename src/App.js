72490ee08699b2c4d4421e474d9b2c2960835973
charlotte divine <divinemaina@gmail.com>
2022-10-24 18:55:00 -0700

    add new tour form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
-import MainPage from './components/MainPage';
+
 import './styles/index.css';
 import DeleteTours from './components/DeleteTours';
+import CreateTour from './components/CreateTour';
 
 function App() {
   return (
     <>
-      <MainPage />
       <div className="app">
         <Nav />
 
         <Routes>
           <Route path="/DeleteTours" element={<DeleteTours />} />
+          <Route path="/CreateTour" element={<CreateTour />} />
         </Routes>
       </div>
     </>
