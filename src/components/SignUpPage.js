8a2bb085950d1e51164c5dbd2a7caa33551370b0
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-10-29 22:09:08 +0500

    Updated Sign Up component and functionality.

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,16 +1,68 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
+import { signup } from '../redux/user/userAPI';
 import '../styles/SignUpPage.css';
 
 const SignUp = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [value, setValue] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const onChange = (e) => {
+    setValue({
+      ...value,
+      [e.target.id]: e.target.value,
+    });
+    console.log(value);
+  };
+
+  const createUser = (e) => {
+    e.preventDefault();
+    dispatch(signup(value, navigate));
+    // navigate('/login');
+    // if (dispatch(signup(value))) {
+    //   navigate('/login');
+    // }
+  };
+
+  useEffect(() => {
+    document.title = 'Register';
+  }, []);
+
   return (
     <div className="signup">
       <h1>Sign Up</h1>
-      <form className="signup-form">
-        <input type="text" placeholder="Name" required />
-        <input type="email" placeholder="Email" required />
-        <input type="password" placeholder="Password" required />
-        <button type="submit" value="add-tour">Register</button>
+      <form className="signup-form" onSubmit={createUser}>
+        <input
+          type="text"
+          placeholder="Name"
+          id="name"
+          required
+          onChange={onChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          required
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          required
+          onChange={onChange}
+        />
+        <button type="submit" value="add-tour">
+          Register
+        </button>
       </form>
       <div className="form-links">
         <h3>Do you have an account?</h3>
