84944ce0278825966e4831feb233c910164a2e40
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-10-30 22:04:38 +0500

    Updated Tour Details Page.

--- src/components/TourDetails.js
+++ src/components/TourDetails.js
@@ -1,38 +1,51 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable object-curly-newline */
-import React from 'react';
+import { React, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router';
 import '../styles/tourDetails.css';
 
 import { FiSettings } from 'react-icons/fi';
 import { AiFillRightCircle } from 'react-icons/ai';
-import tour3 from '../assets/images/tour3.jpeg';
+import { GetToursAPI } from '../redux/tours/toursAPI';
+// import tour3 from '../assets/images/tour3.jpeg';
 
 const TourDetails = () => {
+  const { tourID } = useParams();
+  const dispatch = useDispatch();
+  const Store = useSelector((store) => store.tours);
+  useEffect(() => {
+    document.title = dispatch(GetToursAPI(tourID));
+  }, [dispatch, tourID]);
+  const tour = Store[0];
+
+  useEffect(() => {
+    document.title = 'Tour Details';
+  }, []);
+
   return (
     <div className="tour-details-container">
       <div className="tour-image-container">
-        <img className="tours-image" src={tour3} alt="Tour" />
+        <img className="tours-image" src={tour.photo} alt="Tour" />
       </div>
 
       <div className="tour-details">
-        <h1 className="tour-heading">NORTHEN LIGHTS</h1>
-        <p className="tour-exp">
-          This is an amazing place in North of Canada which gathers people
-          attention from all over the World.
-        </p>
+        <h1 className="tour-heading">{tour.title}</h1>
+        <p className="tour-exp">{tour.description}</p>
         <ul className="specifications">
           <li className="spec">
             City
-            <span className="spec-info">Makkah</span>
+            <span className="spec-info">{tour.city}</span>
           </li>
           <li className="spec">
             Duration(weeks)
-            <span className="spec-info">5.0</span>
+            <span className="spec-info">{tour.duration}</span>
           </li>
           <li className="spec">
             Cost
-            <span className="spec-info">$500</span>
+            <span className="spec-info">{tour.cost}</span>
           </li>
           <li className="spec">
             Available Now
@@ -41,7 +54,7 @@ const TourDetails = () => {
         </ul>
         <button type="button" className="reserve-button">
           <FiSettings className="reserve-symbol-1" />
-          Reserve
+          <Link to="/tours/reserve">Reserve</Link>
           <AiFillRightCircle className="reserve-symbol-2" />
         </button>
       </div>
