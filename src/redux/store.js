bf0f0f53ac0483655323b2f051dcb9316309ee06
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-10-30 18:54:52 +0500

    Created Action Creators and Reducer for Tours.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 /* eslint-disable */
 
 import { configureStore } from '@reduxjs/toolkit';
+import { tourReducer } from './tours/tours';
 import { userReducer } from './user/user';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    tours: tourReducer,
   },
 });
+
+export default store;
---
+++ src/redux/tours/toursAPI.js
@@ -0,0 +1,50 @@
+import { getApiDataTour, getApiDataTourDetail, removeTour } from './tours';
+
+const URL = 'https://tourify-app.herokuapp.com/tours';
+
+export const fetchApiDataTours = () => async (dispatch) => {
+  const result = await fetch(URL);
+  const resultJson = await result.json();
+  if (result.status === 200) {
+    dispatch(getApiDataTour(resultJson));
+  }
+};
+
+export const GetToursAPI = (num) => async (dispatch) => {
+  const response = await fetch(URL.concat('/').concat(num), {});
+  const tours = await response.json();
+  dispatch(getApiDataTourDetail(tours));
+};
+
+export const postTourApi = (newTour, token) => async () => {
+  const response = await fetch(URL, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(newTour),
+  });
+  if (response.status === 201) {
+    fetchApiDataTours();
+  }
+};
+
+export const deleteTourApi = (id, token) => async (dispatch) => {
+  const response = await fetch(`${URL}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  });
+  if (response.status === 204) {
+    dispatch(removeTour(id));
+  }
+};
