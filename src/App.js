a78cbd1b800ed86a8e7a36d06a789a934cb45e41
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-11-02 21:35:33 +0500

    Added Lazy Loading for Main Page.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 /* eslint-disable import/no-named-as-default-member */
 import React, { useEffect, lazy, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
-
 import { Route, Routes } from 'react-router-dom';
 import CryptoJS from 'crypto-js';
-
 import { addUSER } from './redux/user/user';
 import Loader from './components/Loader';
 import Nav from './components/Nav';
-// import MainPage from './components/MainPage';
 import DeleteTours from './components/DeleteTours';
 import TourDetails from './components/TourDetails';
 import CreateTour from './components/CreateTour';
 import Reservation from './components/Reservation';
 import SignUpPage from './components/SignUpPage';
 import LogInPage from './components/LogInPage';
+import ShowReservation from './components/ShowReservation';
 import './styles/index.css';
 
 const MainPage = lazy(() => import('./components/MainPage'));
@@ -33,21 +31,19 @@ function App() {
 
   return (
     <>
-      {/* <Router> */}
       <Nav />
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/l" element={<Loader />} />
           <Route path="/tours/:tourID" element={<TourDetails />} />
           <Route path="/tours/delete" element={<DeleteTours />} />
           <Route path="/tours/create" element={<CreateTour />} />
-          <Route path="/tours/reserve" element={<Reservation />} />
+          <Route path="/tours/reservation-form" element={<Reservation />} />
+          <Route path="/tours/reservations" element={<ShowReservation />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/login" element={<LogInPage />} />
         </Routes>
       </Suspense>
-      {/* </Router> */}
     </>
   );
 }
